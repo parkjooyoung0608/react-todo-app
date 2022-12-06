ab39239de14d3168acc2bfcaf687a409887de6b0
parkjooyoung <mirkduddl@naver.com>
2022-12-06 23:04:42 +0900

    section2 : Map메소드를 사용한 할 일 목록 나열

--- src/App.js
+++ src/App.js
@@ -19,6 +19,19 @@ export default class App extends Component {
     };
   };
 
+  todoData = [
+    {
+      id: "1",
+      title: "공부하기",
+      completed: true,
+    },
+    {
+      id: "2",
+      title: "청소하기",
+      completed: false,
+    },
+  ];
+
   render() {
     return (
       <div className="container">
@@ -26,12 +39,13 @@ export default class App extends Component {
           <div className="title">
             <h1>할 일 목록</h1>
           </div>
-
-          <div style={this.getStyle()}>
-            <input type="checkbox" defaultChecked={false} />
-            공부하기
-            <button style={this.btnStyle}>X</button>
-          </div>
+          {this.todoData.map((data) => (
+            <div style={this.getStyle()} key={data.id}>
+              <input type="checkbox" defaultChecked={false} />
+              {data.title}
+              <button style={this.btnStyle}>X</button>
+            </div>
+          ))}
         </div>
       </div>
     );
