69b373a76023ff1586975aa74913ddcaa1c03c6c
parkjooyoung <mirkduddl@naver.com>
2022-12-07 00:13:51 +0900

    section2 : 할 일 목록 추가하기

--- src/App.js
+++ src/App.js
@@ -15,6 +15,8 @@ export default class App extends Component {
         completed: false,
       },
     ],
+
+    value: "",
   };
 
   btnStyle = {
@@ -39,6 +41,27 @@ export default class App extends Component {
     this.setState({ todoData: newTodoData });
   };
 
+  handleChange = (e) => {
+    console.log(e);
+    this.setState({ value: e.target.value });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    // 새로운 할 일 데이터
+    let newTodo = {
+      id: Date.now(),
+      title: this.state.value,
+      completed: false,
+    };
+
+    // 원래 있던 할 일에 새로운 할 일 더해주기
+    this.setState({ todoData: [...this.state.todoData, newTodo] });
+
+    this.setState({ value: "" });
+  };
+
   render() {
     return (
       <div className="container">
@@ -46,6 +69,7 @@ export default class App extends Component {
           <div className="title">
             <h1>할 일 목록</h1>
           </div>
+
           {this.state.todoData.map((data) => (
             <div style={this.getStyle()}>
               <input type="checkbox" defaultChecked={false} />
@@ -58,6 +82,23 @@ export default class App extends Component {
               </button>
             </div>
           ))}
+
+          <form style={{ display: "flex" }} onSubmit={this.handleSubmit}>
+            <input
+              type="text"
+              name="value"
+              style={{ flex: "10", padding: "5px" }}
+              placeholder="해야 할 일을 입력하세요."
+              value={this.state.value}
+              onChange={this.handleChange}
+            />
+            <input
+              type="submit"
+              value="입력"
+              className="btn"
+              style={{ flex: "1" }}
+            />
+          </form>
         </div>
       </div>
     );
