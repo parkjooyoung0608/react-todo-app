2493749df823a58fc414cf7d487505dc8e6c81ac
parkjooyoung <mirkduddl@naver.com>
2022-12-07 00:44:09 +0900

    section2 : 마무리 된 일 표시하기(조건부 삼항 연산자)

--- src/App.js
+++ src/App.js
@@ -3,19 +3,7 @@ import "./App.css";
 
 export default class App extends Component {
   state = {
-    todoData: [
-      {
-        id: "1",
-        title: "공부하기",
-        completed: true,
-      },
-      {
-        id: "2",
-        title: "청소하기",
-        completed: false,
-      },
-    ],
-
+    todoData: [],
     value: "",
   };
 
@@ -28,11 +16,11 @@ export default class App extends Component {
     float: "right",
   };
 
-  getStyle = () => {
+  getStyle = (completed) => {
     return {
       padding: "10px",
       borderBottom: "1px #ccc dotted",
-      textDecoration: "none",
+      textDecoration: completed ? "line-through" : "none",
     };
   };
 
@@ -57,9 +45,18 @@ export default class App extends Component {
     };
 
     // 원래 있던 할 일에 새로운 할 일 더해주기
-    this.setState({ todoData: [...this.state.todoData, newTodo] });
+    this.setState({ todoData: [...this.state.todoData, newTodo], value: "" });
+  };
 
-    this.setState({ value: "" });
+  handleCompleteChange = (id) => {
+    let newTodoData = this.state.todoData.map((data) => {
+      if (data.id === id) {
+        data.completed = !data.completed;
+      }
+      return data;
+    });
+
+    this.setState({ todoData: newTodoData });
   };
 
   render() {
@@ -71,8 +68,12 @@ export default class App extends Component {
           </div>
 
           {this.state.todoData.map((data) => (
-            <div style={this.getStyle()}>
-              <input type="checkbox" defaultChecked={false} />
+            <div style={this.getStyle(data.completed)}>
+              <input
+                type="checkbox"
+                defaultChecked={false}
+                onChange={() => this.handleCompleteChange(data.id)}
+              />
               {data.title}
               <button
                 style={this.btnStyle}
