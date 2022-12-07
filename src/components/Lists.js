9cfab7bf8b4d9829cd9bf8c2e16419b465eafe24
parkjooyoung <mirkduddl@naver.com>
2022-12-07 23:12:01 +0900

    section3 : TailWindCss로 스타일링 하기

--- src/components/Lists.js
+++ src/components/Lists.js
@@ -1,23 +1,6 @@
 import React from "react";
 
 const Lists = ({ todoData, setTodoData }) => {
-  const btnStyle = {
-    color: "#fff",
-    border: "none",
-    padding: "5px 9px",
-    borderRadius: "50%",
-    cursor: "pointer",
-    float: "right",
-  };
-
-  const getStyle = (completed) => {
-    return {
-      padding: "10px",
-      borderBottom: "1px #ccc dotted",
-      textDecoration: completed ? "line-through" : "none",
-    };
-  };
-
   const handleClick = (id) => {
     const newTodoData = todoData.filter((data) => data.id !== id);
     setTodoData(newTodoData);
@@ -36,16 +19,27 @@ const Lists = ({ todoData, setTodoData }) => {
   return (
     <div>
       {todoData.map((data) => (
-        <div style={getStyle(data.completed)} key={data.id}>
-          <input
-            type="checkbox"
-            defaultChecked={false}
-            onChange={() => handleCompleteChange(data.id)}
-          />
-          {data.title}
-          <button style={btnStyle} onClick={() => handleClick(data.id)}>
-            X
-          </button>
+        <div key={data.id}>
+          <div className="flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 bg-gray-100 border rounded">
+            <div className="items-center">
+              <input
+                type="checkbox"
+                defaultChecked={false}
+                onChange={() => handleCompleteChange(data.id)}
+              />
+              <span className={data.completed ? "line-through" : undefined}>
+                {data.title}
+              </span>
+            </div>
+            <div className="items-center">
+              <button
+                className="px-4 py-2"
+                onClick={() => handleClick(data.id)}
+              >
+                X
+              </button>
+            </div>
+          </div>
         </div>
       ))}
     </div>
